3c4d7f9bbcef234fb77ec4bdada69fa9a2c2529a
deyperfect <edrylpalinis@gmail.com>
2026-01-25 21:36:18 +0800

    Fixed bug in challenges.js

--- src/pages/coding-challenges/challenges.js
+++ src/pages/coding-challenges/challenges.js
@@ -1,3 +1,26 @@
+// Theme toggler
+let darkmode = localStorage.getItem('dark');
+const switchTheme = document.getElementById('theme-toggle')
+
+const darkModeOn = () => {
+    document.body.classList.add('dark');
+    localStorage.setItem('dark', 'active');
+}
+
+const lightModeOn = () => {
+    document.body.classList.remove('dark');
+    localStorage.setItem('dark', null);
+}
+
+switchTheme.addEventListener('click', () => {
+    darkmode = localStorage.getItem('dark');
+    if (darkmode !== 'active') {
+        darkModeOn();
+    } else {
+        lightModeOn();
+    }
+});
+
 window.addEventListener('scroll', callback => {
 
 const footer = document.querySelector('.footer');
