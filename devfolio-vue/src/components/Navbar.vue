4eca135ff3a902574dd375ddf5bc19540432add3
deyperfect <edrylpalinis@gmail.com>
2026-02-22 10:52:27 +0800

    Adjusted navbar links

--- devfolio-vue/src/components/Navbar.vue
+++ devfolio-vue/src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <!-- Navbar Start -->
         <nav class="navbar navbar-expand-lg fixed-top" id="mynavbar">
             <div class="container mx-auto">
-                <a class="navbar-brand" href="#landing">EP</a>
+                <RouterLink class="navbar-brand" :to="{ name: 'Home' }">EP</RouterLink>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Open navigation menu">
                     <span class="navbar-toggler-icon"></span>
                 </button>
