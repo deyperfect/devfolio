e6c3de9a67402e51fe1a599e19c81406949bdf33
Edryl Palinis <edrylpalinis@gmail.com>
2026-08-28 19:57:14 +0800

    style: customize section container width for desktop

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,16 +3,15 @@ import technologies from "../data/techStack.json";
 
 const TechStack = () => {
   return (
+    
     <section>
-
-    <div className="container mx-auto">
 
       <div >
         <h2 >Tech Stack</h2>
         <p >What I currently work with</p>
       </div>
 
-      <div className="grid gap-3 grid-cols-12 px-5 justify-center">
+      <div className="grid gap-3 grid-cols-12 px-5 md:px-0 justify-center">
         {technologies.map((technology) => (
             <div key={technology.id} className="col-span-4 md:col-span-3">
                 <TechStackCard technology={technology} />
@@ -21,9 +20,8 @@ const TechStack = () => {
         
       </div>
 
-    </div>
-
-  </section>
+    </section>
+ 
   )
 }
 
