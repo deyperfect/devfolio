098d40e39050fab8abb4174c6817f5c9d5edbc7d
Edryl Palinis <edrylpalinis@gmail.com>
2026-08-31 17:01:51 +0800

    feat: update navbar links for section navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,22 @@ import { IoCloseCircleOutline } from "react-icons/io5";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const closeMenu = () => setIsOpen(false);
-  
+
   return (
     <header>
       <nav className="fixed top-0 z-50 w-full shadow-md" id="mynavbar">
         <div className="container mx-auto xl:max-w-[1300px] flex min-h-16 items-center justify-between px-4 lg:px-0">
-
-          <Link to="/" className="text-xl font-bold" onClick={closeMenu}>
+          <Link
+            to="/"
+            className="text-xl font-bold"
+            onClick={() => {
+              closeMenu();
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
+          >
             Edryl Palinis
           </Link>
 
@@ -23,7 +32,11 @@ const Navbar = () => {
             aria-label="Open navigation menu"
             aria-expanded={isOpen}
           >
-            {isOpen ? <IoCloseCircleOutline size={24} /> : <IoIosMenu size={24} />}
+            {isOpen ? (
+              <IoCloseCircleOutline size={24} />
+            ) : (
+              <IoIosMenu size={24} />
+            )}
           </button>
 
           <div
@@ -36,40 +49,46 @@ const Navbar = () => {
                 <Link
                   to="/"
                   className="block hover:text-gray-500"
-                  onClick={closeMenu}
+                  onClick={() => {
+                    closeMenu();
+                    window.scrollTo({
+                      top: 0,
+                      behavior: "smooth",
+                    });
+                  }}
                 >
                   Home
                 </Link>
               </li>
 
               <li>
-                <Link
-                  to="/#tools"
+                <a
+                  href="#tools"
                   className="block hover:text-gray-500"
                   onClick={closeMenu}
                 >
                   Tech Stack
-                </Link>
+                </a>
               </li>
 
               <li>
-                <Link
-                  to="/#projects"
+                <a
+                  href="#projects"
                   className="block hover:text-gray-500"
                   onClick={closeMenu}
                 >
                   Projects
-                </Link>
+                </a>
               </li>
 
               <li>
-                <Link
-                  to="/#contact"
+                <a
+                  href="#contact"
                   className="block hover:text-gray-500"
                   onClick={closeMenu}
                 >
                   Contact
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
