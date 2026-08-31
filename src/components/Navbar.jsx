e81338978eab07960992ac3f55c0ffe3fb40fa5b
Edryl Palinis <edrylpalinis@gmail.com>
2026-08-31 16:47:16 +0800

    style: set max width on desktop to 1300px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <header>
       <nav className="fixed top-0 z-50 w-full shadow-md" id="mynavbar">
-        <div className="container mx-auto flex min-h-16 items-center justify-between px-4">
+        <div className="container mx-auto xl:max-w-[1300px] flex min-h-16 items-center justify-between px-4 lg:px-0">
 
           <Link to="/" className="text-xl font-bold" onClick={closeMenu}>
             Edryl Palinis
