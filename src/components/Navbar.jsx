e1df3da1073c88323d3e8c7694c8779aa2b082b6
Edryl Palinis <edrylpalinis@gmail.com>
2026-09-01 23:46:43 +0800

    feat: render NavLinks component inside Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoIosMenu } from "react-icons/io";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,7 +11,7 @@ const Navbar = () => {
   return (
     <header>
       <nav
-        className="fixed top-0 z-50 w-full lg:shadow-md bg-primary"
+        className="fixed top-0 z-50 w-full lg:shadow-md bg-transparent lg:bg-primary"
         id="mynavbar"
       >
         <div className="container mx-auto xl:max-w-[1300px] flex min-h-16 items-center justify-between px-4 lg:px-0">
@@ -30,38 +31,7 @@ const Navbar = () => {
           {/* Desktop Navigation Links */}
           <div className="hidden lg:block">
             <ul className="flex items-center gap-4 font-semibold">
-              <li>
-                <Link
-                  to="/"
-                  className="nav-link"
-                  onClick={() => {
-                    window.scrollTo({
-                      top: 0,
-                      behavior: "smooth",
-                    });
-                  }}
-                >
-                  Home
-                </Link>
-              </li>
-
-              <li>
-                <a href="#tools" className="nav-link">
-                  Tech Stack
-                </a>
-              </li>
-
-              <li>
-                <a href="#projects" className="nav-link">
-                  Projects
-                </a>
-              </li>
-
-              <li>
-                <a href="#contact" className="nav-link">
-                  Contact
-                </a>
-              </li>
+              <NavLinks />
             </ul>
           </div>
 
@@ -97,39 +67,7 @@ const Navbar = () => {
             } sidebar`}
           >
             <ul className="flex flex-col lg:items-center gap-4 px-1 lg:flex-row lg:p-0 font-semibold">
-              <li>
-                <Link
-                  to="/"
-                  className="nav-link"
-                  onClick={() => {
-                    closeMenu();
-                    window.scrollTo({
-                      top: 0,
-                      behavior: "smooth",
-                    });
-                  }}
-                >
-                  Home
-                </Link>
-              </li>
-
-              <li>
-                <a href="#tools" className="nav-link" onClick={closeMenu}>
-                  Tech Stack
-                </a>
-              </li>
-
-              <li>
-                <a href="#projects" className="nav-link" onClick={closeMenu}>
-                  Projects
-                </a>
-              </li>
-
-              <li>
-                <a href="#contact" className="nav-link" onClick={closeMenu}>
-                  Contact
-                </a>
-              </li>
+              <NavLinks closeMenu={closeMenu} />
             </ul>
           </div>
         </div>
